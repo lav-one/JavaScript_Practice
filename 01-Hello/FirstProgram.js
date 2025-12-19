7ce91cc02cd4c56eb13b029e2e64c566c0b122e6
lavanyamutyala <lmutyala@tekion.com>
2025-12-19 11:45:48 +0530

    Variables

---
+++ 01-Hello/FirstProgram.js
@@ -0,0 +1,16 @@
+// console.log("Hello World!!")
+
+// let num = 4;
+// console.log(num);
+
+// num = 9;
+// console.log(num);
+
+let userName = "App \vle";
+console.log(userName);
+
+// let radius = 5;
+// const pi = 3.14;
+
+// let area = pi * radius * radius;
+// console.log(area);
---
+++ 02-Basics/01_Variables.js
@@ -0,0 +1,13 @@
+var fullName = "Lavanya Mutyala";
+var courseName = "JavaScript";
+var isLoggedIn = false;
+var loggedCount = 0;
+var paymentMode;
+
+console.log(fullName);
+console.log(courseName);
+console.log(loggedCount);
+console.log(isLoggedIn);
+
+paymentMode = "Credit Card";
+
